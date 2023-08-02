80ad297c702c61cb0eed3be69cb41121a2266982
Yohannes Mulu <dev.yohannes@gmail.com>
2023-08-02 12:42:03 +0300

    new description and added pako style

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-var restaurant_name = document.getElementById("title");
+var restaurant_name = document.querySelector("#title");
+var desc = document.querySelector(".discription");
 
+desc.innerHTML = "A <i>place</i> to eat";
 restaurant_name.innerHTML = "<h1>PAKO</h1>";
