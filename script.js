ae617811a9394e3d5c90edb256e136e8d5177945
Yohannes Mulu <dev.yohannes@gmail.com>
2023-08-02 13:09:16 +0300

    adding select menus

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 var restaurant_name = document.querySelector("#title");
 var desc = document.querySelector(".discription");
 var body = document.querySelector("body");
+var foods = document.querySelector("#foods");
 
 desc.innerHTML = "A <i>place</i> to eat";
 restaurant_name.innerHTML = "<h1>PAKO</h1>";
 
-var ul = document.createElement("ul");
-body.appendChild(ul);
+var food_list = [
+  "Changu fish",
+  "Ugali",
+  "Egusi soup",
+  "Eba",
+  "Misr wot",
+  "Gomen",
+];
 
-for (var i = 0; i < 10; i++) {
-  var li = document.createElement("li");
-  li.innerHTML = "hello there number" + i;
-  ul.appendChild(li);
+for (var i = 0; i < food_list.length; i++) {
+  var newoption = document.createElement("option");
+  newoption.value = food_list[i];
+  newoption.innerHTML = food_list[i];
+  foods.appendChild(newoption);
 }
-
-var newestdiv = document.createElement("div");
-newestdiv.innerHTML = "abc";
-restaurant_name.appendChild(newestdiv);
